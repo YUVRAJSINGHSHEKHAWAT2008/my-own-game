d53f1c61bbd8e456008388bfeab39a26be1e87fa
YUVRAJSINGHSHEKHAWAT2008 <76441512+YUVRAJSINGHSHEKHAWAT2008@users.noreply.github.com>
2021-04-10 20:57:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,11 @@
+var player1,player1I;
+var player2,player2I;
+var player3,player3I;
+var backGround,backgroundImage;
+var light1,light1I
+var light2,light2I
+var light3,light3I
+
+function preload(){
+    player1I = loadImage("tron1")
+}
